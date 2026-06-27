888abb39a77d4ccec30e875dce5d3bea988c3d5d
NgMinhDucc <ducnm@gdscptit.dev>
2026-06-27 22:40:45 +0700

    June 27 2026

---
+++ Easy/leetcode14.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        lcp = ""
+        for i in range(strs[0]):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return lcp
+            lcp += strs[0][i]
+        return lcp
+
+       
+solve = Solution() 
+for _ in range(2):
+    lis = input().split()
+    print(solve.longestCommonPrefix(lis))
