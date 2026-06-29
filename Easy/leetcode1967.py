b8addc08199e927067843cfee1bb538a068dbfbc
NgMinhDucc <ducnm@gdscptit.dev>
2026-06-29 08:53:11 +0700

    June 29 2026

---
+++ Easy/leetcode1967.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numOfStrings(self, patterns: list[str], word: str) -> int:
+        cnt = 0
+        for pat in patterns:
+            if pat in word:
+                cnt += 1
+        return cnt
+    
+solve = Solution()
+for _ in range(int(input())):
+    patterns = input().split()
+    word = input()
+    print(solve.numOfStrings(patterns, word))
